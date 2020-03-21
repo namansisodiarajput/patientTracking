36f9f55fe304ccb1ce7ee3b76c3e79aff0b2b0d0
namansisodia <namansisodia619@gmail.com>
2020-03-21 18:05:00 +0530

    added crud api

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,7 @@
 const User = require('../models').User;
 
 module.exports = {
+
   create(req, res) {
     return User
       .create({
@@ -13,6 +14,53 @@ module.exports = {
         longitude: req.body.longitude
       })
       .then(user => res.status(201).send(user))
+      .catch(error => res.status(400).send(error));
+  },
+
+  getUser(req, res) {
+      return User
+        .findByPk(req.params.userId)
+        .then(user => {
+          if (!user) {
+            return res.status(404).send({
+              message: 'User Not Found',
+            });
+          }
+          return res.status(200).send(user);
+        })
+        .catch(error => res.status(400).send(error));
+  },
+
+  updateUser(req, res) {
+      return User
+        .findByPk(req.params.userId)
+        .then(user => {
+          if (!user) {
+            return res.status(404).send({
+              message: 'User Not Found',
+            });
+          }
+          return user
+            .update({
+              name: req.body.name || user.name,
+              address: req.body.address || user.address,
+              mobileNumber: req.body.mobileNumber || user.mobileNumber,
+              emailId: req.body.emailId || user.emailId,
+              password: req.body.password || user.password,
+              latitude: req.body.latitude || user.latitude,
+              longitude: req.body.longitude || user.longitude
+            })
+            .then(() => res.status(200).send(user))  // Send back the updated todo.
+            .catch((error) => res.status(400).send(error));
+        })
+        .catch((error) => res.status(400).send(error));
+    },
+
+  list(req, res) {
+    console.log(User);
+    return User
+      .findAll()
+      .then(users => res.status(200).send(users))
       .catch(error => res.status(400).send(error));
   },
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,11 @@ const userController = require('../controllers').users;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to the Todos API!',
+    message: 'Welcome to the Niramai!',
   }));
 
   app.post('/api/register', userController.create);
+  app.get('/api/users/list', userController.list);
+  app.get('/api/users/:userId', userController.getUser);
+  app.put('/api/users/:userId', userController.updateUser);
 };
